9f5a33db5d378bcb4dc0b9bf1a49bfe955412d3b
Matvey <matviyvac@gmail.com>
2025-02-28 19:05:21 +0200

    fix: optimized pages loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
-import AlbumDetail from './pages/Albums/AlbumDetail'
-import Albums from './pages/Albums/Albums'
-import ArtistDetail from './pages/Artists/ArtistDetail'
-import Artists from './pages/Artists/Artists'
-import Login from './pages/Auth/Login'
-import Register from './pages/Auth/Register'
-import Favorites from './pages/Favorites/Favorites'
-import Home from './pages/Home/Home'
-import PlaylistDetail from './pages/Playlists/PlaylistDetail'
-import Playlists from './pages/Playlists/Playlists'
-import Tracks from './pages/Tracks/Tracks'
 import { setUser } from './redux/slices/authSlice'
 import { AppDispatch } from './redux/store'
+
+const Home = React.lazy(() => import('./pages/Home/Home'))
+const Albums = React.lazy(() => import('./pages/Albums/Albums'))
+const AlbumDetail = React.lazy(() => import('./pages/Albums/AlbumDetail'))
+const Artists = React.lazy(() => import('./pages/Artists/Artists'))
+const ArtistDetail = React.lazy(() => import('./pages/Artists/ArtistDetail'))
+const Tracks = React.lazy(() => import('./pages/Tracks/Tracks'))
+const Playlists = React.lazy(() => import('./pages/Playlists/Playlists'))
+const PlaylistDetail = React.lazy(
+	() => import('./pages/Playlists/PlaylistDetail')
+)
+const Login = React.lazy(() => import('./pages/Auth/Login'))
+const Register = React.lazy(() => import('./pages/Auth/Register'))
+const Favorites = React.lazy(() => import('./pages/Favorites/Favorites'))
 
 function App() {
 	const dispatch = useDispatch<AppDispatch>()
@@ -30,17 +33,98 @@ function App() {
 		<div className='max-w-[1400px] mx-auto px-[15px]'>
 			<Routes>
 				<Route path='/' element={<MainLayout />}>
-					<Route path='' element={<Home />} />
-					<Route path='/albums' element={<Albums />} />
-					<Route path='/albums/:id' element={<AlbumDetail />} />
-					<Route path='/artists' element={<Artists />} />
-					<Route path='/artists/:id' element={<ArtistDetail />} />
-					<Route path='/tracks' element={<Tracks />} />
-					<Route path='/playlists' element={<Playlists />} />
-					<Route path='/playlists/:id' element={<PlaylistDetail />} />
-					<Route path='login' element={<Login />} />
-					<Route path='register' element={<Register />} />
-					<Route path='favorites' element={<Favorites />} />
+					<Route
+						path=''
+						element={
+							<React.Suspense fallback={<div>Loading Home Page...</div>}>
+								<Home />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='/albums'
+						element={
+							<React.Suspense fallback={<div>Loading Albums Page...</div>}>
+								<Albums />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='/albums/:id'
+						element={
+							<React.Suspense
+								fallback={<div>Loading Album Detail Page...</div>}
+							>
+								<AlbumDetail />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='/artists'
+						element={
+							<React.Suspense fallback={<div>Loading Artists Page...</div>}>
+								<Artists />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='/artists/:id'
+						element={
+							<React.Suspense fallback={<div>Loading Artist Detail Page</div>}>
+								<ArtistDetail />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='/tracks'
+						element={
+							<React.Suspense fallback={<div>Loading Tracks Page...</div>}>
+								<Tracks />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='/playlists'
+						element={
+							<React.Suspense fallback={<div>Loading Playlists Page...</div>}>
+								<Playlists />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='/playlists/:id'
+						element={
+							<React.Suspense
+								fallback={<div>Loading Playlist Detail Page...</div>}
+							>
+								<PlaylistDetail />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='login'
+						element={
+							<React.Suspense fallback={<div>Loading Login Page...</div>}>
+								<Login />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='register'
+						element={
+							<React.Suspense fallback={<div>Loading Register Page...</div>}>
+								<Register />
+							</React.Suspense>
+						}
+					/>
+					<Route
+						path='favorites'
+						element={
+							<React.Suspense fallback={<div>Loading Favorites Page...</div>}>
+								<Favorites />
+							</React.Suspense>
+						}
+					/>
 				</Route>
 			</Routes>
 		</div>
